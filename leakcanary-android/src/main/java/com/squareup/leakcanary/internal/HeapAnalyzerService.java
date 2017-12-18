6a76883f33d269a3f87d39c326d754f6c19269a7
shmuelr <sdrosansky@gmail.com>
2017-12-18 15:35:15 -0500

    Whoops, cleanup unused import

--- leakcanary-android/src/main/java/com/squareup/leakcanary/internal/HeapAnalyzerService.java
+++ leakcanary-android/src/main/java/com/squareup/leakcanary/internal/HeapAnalyzerService.java
@@ -16,7 +16,6 @@
 package com.squareup.leakcanary.internal;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
