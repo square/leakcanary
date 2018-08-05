7f305b9bf3b9cbb49f478c6b177ba35969803248
Pierre-Yves Ricau <py@squareup.com>
2018-08-05 11:23:04 -0700

    Removing the LeakCanary toast
    - We now have a notification for all the stages of leak detection, including heap dumping
    - Using proper Android toasts crashes in recent Android versions (#844)
    - Custom Toast display doesn't work on recent Android versions (#1062)
    - The toast was also annoying if the app isn't in the foreground (#1069)
    - Waiting for the toast to display was done by waiting for main thread idle which added an unnecessary delay.
    
    Fixes #1062
    Fixes #1069

--- leakcanary-android/src/main/java/com/squareup/leakcanary/AndroidHeapDumper.java
+++ leakcanary-android/src/main/java/com/squareup/leakcanary/AndroidHeapDumper.java
@@ -18,38 +18,19 @@ package com.squareup.leakcanary;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.res.Configuration;
-import android.graphics.PixelFormat;
-import android.os.Build;
 import android.os.Debug;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.MessageQueue;
 import android.os.SystemClock;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityManager;
-import android.widget.Toast;
-import com.squareup.leakcanary.internal.FutureResult;
 import com.squareup.leakcanary.internal.LeakCanaryInternals;
 import java.io.File;
-
-import static android.view.accessibility.AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 public final class AndroidHeapDumper implements HeapDumper {
 
   private final Context context;
   private final LeakDirectoryProvider leakDirectoryProvider;
-  private final Handler mainHandler;
 
   public AndroidHeapDumper(Context context, LeakDirectoryProvider leakDirectoryProvider) {
     this.leakDirectoryProvider = leakDirectoryProvider;
     this.context = context.getApplicationContext();
-    mainHandler = new Handler(Looper.getMainLooper());
   }
 
   @SuppressWarnings("ReferenceEquality") // Explicitly checking for named null.
@@ -57,14 +38,6 @@ public final class AndroidHeapDumper implements HeapDumper {
     File heapDumpFile = leakDirectoryProvider.newHeapDumpFile();
 
     if (heapDumpFile == RETRY_LATER) {
-      return RETRY_LATER;
-    }
-
-    FutureResult<Toast> waitingForToast = new FutureResult<>();
-    showToast(waitingForToast);
-
-    if (!waitingForToast.wait(5, SECONDS)) {
-      CanaryLog.d("Did not dump heap, too much time waiting for Toast.");
       return RETRY_LATER;
     }
 
@@ -76,10 +49,8 @@ public final class AndroidHeapDumper implements HeapDumper {
     int notificationId = (int) SystemClock.uptimeMillis();
     notificationManager.notify(notificationId, notification);
 
-    Toast toast = waitingForToast.get();
     try {
       Debug.dumpHprofData(heapDumpFile.getAbsolutePath());
-      cancelToast(toast);
       notificationManager.cancel(notificationId);
       return heapDumpFile;
     } catch (Exception e) {
@@ -88,109 +59,4 @@ public final class AndroidHeapDumper implements HeapDumper {
       return RETRY_LATER;
     }
   }
-
-  private void showToast(final FutureResult<Toast> waitingForToast) {
-    mainHandler.post(new Runnable() {
-      @Override public void run() {
-        final Toast toast = new Toast(context);
-        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-        toast.setDuration(Toast.LENGTH_LONG);
-        LayoutInflater inflater = LayoutInflater.from(context);
-        toast.setView(inflater.inflate(R.layout.leak_canary_heap_dump_toast, null));
-        show(toast);
-        // Waiting for Idle to make sure Toast gets rendered.
-        Looper.myQueue().addIdleHandler(new MessageQueue.IdleHandler() {
-          @Override public boolean queueIdle() {
-            waitingForToast.set(toast);
-            return false;
-          }
-        });
-      }
-    });
-  }
-
-  private void cancelToast(final Toast toast) {
-    mainHandler.post(new Runnable() {
-      @Override public void run() {
-        hide(toast);
-      }
-    });
-  }
-
-  private void show(Toast toast) {
-    View view = toast.getView();
-    Context context = toast.getView().getContext();
-
-    WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-
-    // We can resolve the Gravity here by using the Locale for getting
-    // the layout direction
-    Configuration config = view.getContext().getResources().getConfiguration();
-    int gravity = toast.getGravity();
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      gravity = Gravity.getAbsoluteGravity(gravity, config.getLayoutDirection());
-    }
-
-    WindowManager.LayoutParams params = buildLayoutParams();
-    params.gravity = gravity;
-    if ((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.FILL_HORIZONTAL) {
-      params.horizontalWeight = 1.0f;
-    }
-    if ((gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.FILL_VERTICAL) {
-      params.verticalWeight = 1.0f;
-    }
-    params.x = toast.getXOffset();
-    params.y = toast.getYOffset();
-    params.verticalMargin = toast.getVerticalMargin();
-    params.horizontalMargin = toast.getHorizontalMargin();
-    params.packageName = context.getPackageName();
-    try {
-      windowManager.addView(view, params);
-    } catch (WindowManager.BadTokenException ignored) {
-      CanaryLog.d("Could not show leak toast, the window token has been canceled");
-      return;
-    }
-    trySendAccessibilityEvent(view);
-  }
-
-  private void hide(Toast toast) {
-    View view = toast.getView();
-    if (view.getParent() != null) {
-      Context context = toast.getView().getContext();
-      WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-      windowManager.removeView(view);
-    }
-  }
-
-  private WindowManager.LayoutParams buildLayoutParams() {
-    WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-    params.height = WindowManager.LayoutParams.WRAP_CONTENT;
-    params.width = WindowManager.LayoutParams.WRAP_CONTENT;
-    params.format = PixelFormat.TRANSLUCENT;
-    params.windowAnimations = android.R.style.Animation_Toast;
-    params.type = WindowManager.LayoutParams.TYPE_TOAST;
-    params.setTitle("Toast");
-    params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-        | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
-    return params;
-  }
-
-  private void trySendAccessibilityEvent(View view) {
-    Context context = view.getContext();
-    AccessibilityManager accessibilityManager =
-        (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
-    if (!accessibilityManager.isEnabled()) {
-      return;
-    }
-    // treat toasts as notifications since they are used to
-    // announce a transient piece of information to the user
-    AccessibilityEvent event = AccessibilityEvent.obtain(TYPE_NOTIFICATION_STATE_CHANGED);
-    event.setClassName(getClass().getName());
-    event.setPackageName(context.getPackageName());
-    view.dispatchPopulateAccessibilityEvent(event);
-    accessibilityManager.sendAccessibilityEvent(event);
-  }
-
-
 }
