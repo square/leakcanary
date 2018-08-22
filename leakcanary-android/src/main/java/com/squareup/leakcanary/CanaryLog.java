a6b6960d73a1da9661f4e85254570bf5f867f954
Zac Sweers <pandanomic@gmail.com>
2018-08-22 00:37:33 -0700

    Error prone 2.3.1 + suggested fix and specify version
    See http://errorprone.info/bugpattern/StringSplitter

--- leakcanary-android/src/main/java/com/squareup/leakcanary/CanaryLog.java
+++ leakcanary-android/src/main/java/com/squareup/leakcanary/CanaryLog.java
@@ -20,7 +20,7 @@ public final class CanaryLog {
       if (formatted.length() < 4000) {
         Log.d("LeakCanary", formatted);
       } else {
-        String[] lines = formatted.split("\n");
+        String[] lines = formatted.split("\n", -1);
         for (String line : lines) {
           Log.d("LeakCanary", line);
         }
