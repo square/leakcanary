db4f4ed56149ca8ea194e4520c5ae92229f60764
Colin Marsch <cmarsch@squareup.com>
2019-04-01 14:41:55 -0700

    Convert leakcanary-sample to Kotlin
    See #1205

---
+++ leakcanary-sample/src/main/java/com/example/leakcanary/ExampleApplication.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.leakcanary
+
+import android.app.Application
+import android.os.Build
+import android.os.StrictMode
+import com.squareup.leakcanary.LeakCanary
+
+open class ExampleApplication : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    setupLeakCanary()
+  }
+
+  protected open fun setupLeakCanary() {
+    enabledStrictMode()
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return
+    }
+    LeakCanary.install(this)
+  }
+
+  companion object {
+    private fun enabledStrictMode() {
+      val builder = StrictMode.ThreadPolicy.Builder()
+      // Disabled DiskReadViolation, see https://github.com/square/leakcanary/issues/1222
+      //    builder.detectDiskReads();
+      builder.detectDiskWrites()
+      builder.detectNetwork()
+      builder.detectCustomSlowCalls()
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        builder.detectResourceMismatches()
+      }
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        builder.detectUnbufferedIo()
+      }
+      builder.penaltyLog()
+      builder.penaltyDeath()
+      StrictMode.setThreadPolicy(builder.build())
+    }
+  }
+}
---
+++ leakcanary-sample/src/test/java/com/example/leakcanary/SampleTest.kt
@@ -0,0 +1,20 @@
+package com.example.leakcanary
+
+import android.view.View
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(application = TestExampleApplication::class)
+class SampleTest {
+  @Test
+  fun testTheThing() {
+    val controller = Robolectric.buildActivity(MainActivity::class.java).create().start().resume().visible()
+    controller.get().findViewById<View>(R.id.async_work).performClick()
+    controller.stop()
+    controller.destroy()
+  }
+}
