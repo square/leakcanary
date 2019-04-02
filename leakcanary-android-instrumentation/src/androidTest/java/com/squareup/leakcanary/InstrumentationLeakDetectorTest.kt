e6f67af4cd3e42e55957ec406ca62de4b993c13c
Colin Marsch <cmarsch@squareup.com>
2019-04-02 16:18:56 -0700

    Convert android-instrumentation tests to Kotlin
    See #1205

---
+++ leakcanary-android-instrumentation/src/androidTest/java/com/squareup/leakcanary/InstrumentationLeakDetectorTest.kt
@@ -0,0 +1,48 @@
+package com.squareup.leakcanary
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.util.Date
+
+/**
+ * Tests that the [InstrumentationLeakDetector] can detect leaks
+ * in instrumentation tests
+ */
+class InstrumentationLeakDetectorTest {
+
+  @Before fun setUp() {
+    LeakCanary.installedRefWatcher()
+        .clearWatchedReferences()
+  }
+
+  @After fun tearDown() {
+    LeakCanary.installedRefWatcher()
+        .clearWatchedReferences()
+  }
+
+  @Test fun detectsLeak() {
+    leaking = Date()
+    val refWatcher = LeakCanary.installedRefWatcher()
+    refWatcher.watch(leaking)
+
+    val leakDetector = InstrumentationLeakDetector()
+    val results = leakDetector.detectLeaks()
+
+    if (results.detectedLeaks.size != 1) {
+      throw AssertionError("Expected exactly one leak, not ${results.detectedLeaks.size}")
+    }
+
+    val firstResult = results.detectedLeaks[0]
+
+    val leakingClassName = firstResult.analysisResult.className
+
+    if (leakingClassName != Date::class.java.name) {
+      throw AssertionError("Expected a leak of Date, not $leakingClassName")
+    }
+  }
+
+  companion object {
+    private var leaking: Any? = null
+  }
+}
