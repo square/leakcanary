ab372ef91bff4b541bb2842acc1f5872b450360b
Pierre-Yves Ricau <py@squareup.com>
2019-04-25 11:32:04 +0200

    Import hprof for analysis
    Also removed the wait for pending files to be removed. Since we have a threshold now, we won't be dumping the heap as often, and we're still using a serial service for the analysis so things will enqueue nicely. This change allows us to reuse the same mechanism for importing hprof even if an analysis is in progress.
    
    Fixes #1040

--- leakcanary-android-core/src/main/java/leakcanary/internal/activity/LeakActivity.kt
+++ leakcanary-android-core/src/main/java/leakcanary/internal/activity/LeakActivity.kt
@@ -3,12 +3,21 @@ package leakcanary.internal.activity
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import com.squareup.leakcanary.core.R
+import leakcanary.CanaryLog
+import leakcanary.HeapDump
+import leakcanary.LeakCanary
+import leakcanary.internal.HeapAnalyzers
+import leakcanary.internal.LeakCanaryUtils
 import leakcanary.internal.activity.db.LeaksDbHelper
 import leakcanary.internal.activity.screen.GroupListScreen
 import leakcanary.internal.navigation.NavigatingActivity
 import leakcanary.internal.navigation.Screen
+import java.io.FileInputStream
+import java.io.IOException
 
 internal class LeakActivity : NavigatingActivity() {
 
@@ -35,6 +44,62 @@ internal class LeakActivity : NavigatingActivity() {
     return GroupListScreen()
   }
 
+  fun requestImportHprof() {
+    val requestFileIntent = Intent(Intent.ACTION_GET_CONTENT).apply {
+      type = "*/*"
+      addCategory(Intent.CATEGORY_OPENABLE)
+    }
+
+    // TODO String res
+    val chooserIntent = Intent.createChooser(requestFileIntent, "Import from")
+    startActivityForResult(chooserIntent, FILE_REQUEST_CODE)
+  }
+
+  override fun onActivityResult(
+    requestCode: Int,
+    resultCode: Int,
+    returnIntent: Intent?
+  ) {
+    CanaryLog.d(
+        "Got activity result with requestCode=$requestCode resultCode=$resultCode returnIntent=$returnIntent"
+    )
+    if (requestCode == FILE_REQUEST_CODE && resultCode == RESULT_OK && returnIntent != null) {
+      returnIntent.data?.let { fileUri ->
+        AsyncTask.THREAD_POOL_EXECUTOR.execute {
+          importHprof(fileUri)
+        }
+      }
+    }
+  }
+
+  private fun importHprof(fileUri: Uri) {
+    try {
+      contentResolver.openFileDescriptor(fileUri, "r")
+          ?.fileDescriptor?.let { fileDescriptor ->
+        val inputStream = FileInputStream(fileDescriptor)
+        LeakCanaryUtils.getLeakDirectoryProvider(this)
+            .newHeapDumpFile()
+            ?.let { target ->
+              inputStream.use { input ->
+                target.outputStream()
+                    .use { output ->
+                      input.copyTo(output, DEFAULT_BUFFER_SIZE)
+                    }
+              }
+              val config = LeakCanary.config
+              val heapDump = HeapDump.builder(target)
+                  .excludedRefs(config.excludedRefs)
+                  .computeRetainedHeapSize(config.computeRetainedHeapSize)
+                  .reachabilityInspectorClasses(config.reachabilityInspectorClasses)
+                  .build()
+              HeapAnalyzers.runAnalysis(this, heapDump)
+            }
+      }
+    } catch (e: IOException) {
+      CanaryLog.d(e, "Could not imported Hprof file")
+    }
+  }
+
   override fun onDestroy() {
     super.onDestroy()
     if (!isChangingConfigurations) {
@@ -53,6 +118,8 @@ internal class LeakActivity : NavigatingActivity() {
   }
 
   companion object {
+    private const val FILE_REQUEST_CODE = 0
+
     fun createPendingIntent(
       context: Context,
       screens: ArrayList<Screen>
