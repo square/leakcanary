608fd685eaab93f69b8eab994cb85965a9b83877
PY <pyricau@users.noreply.github.com>
2019-05-21 10:14:26 +0200

    Remove perflib (#1358)

--- leakcanary-analyzer-perflib/src/main/java/com/squareup/haha/perflib/HahaSpy.kt
+++
@@ -1,8 +0,0 @@
-package com.squareup.haha.perflib
-
-internal fun Instance.allocatingThread(): Instance {
-  val snapshot = mHeap.mSnapshot
-  val threadSerialNumber = if (this is RootObj) mThread else mStack.mThreadSerialNumber
-  val thread = snapshot.getThread(threadSerialNumber)
-  return snapshot.findInstance(thread.mId)
-}
