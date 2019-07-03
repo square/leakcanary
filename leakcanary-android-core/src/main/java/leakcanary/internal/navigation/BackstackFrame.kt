a4fd52606533f89150247f6bc751d5aceb0ea213
PY <pyricau@users.noreply.github.com>
2019-07-03 15:56:33 -0700

    Update compile SDK to API 29 (#1437)

--- leakcanary-android-core/src/main/java/leakcanary/internal/navigation/BackstackFrame.kt
+++ leakcanary-android-core/src/main/java/leakcanary/internal/navigation/BackstackFrame.kt
@@ -15,7 +15,7 @@ internal class BackstackFrame : Parcelable {
   ) {
     this.screen = source.readSerializable() as Screen
     @Suppress("UNCHECKED_CAST")
-    this.viewState = source.readSparseArray(javaClass.classLoader) as SparseArray<Parcelable>?
+    this.viewState = source.readSparseArray<Parcelable>(javaClass.classLoader)
   }
 
   constructor(
