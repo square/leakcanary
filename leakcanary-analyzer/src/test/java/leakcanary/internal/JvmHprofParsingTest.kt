17b8086fcdfea462563f823c210e78264f2ef929
PY <pyricau@users.noreply.github.com>
2019-07-17 12:54:23 -0700

    Add support for JVM Heap dumps (#1469)
    Related to #54

---
+++ leakcanary-analyzer/src/test/java/leakcanary/internal/JvmHprofParsingTest.kt
@@ -0,0 +1,42 @@
+package leakcanary.internal
+
+import leakcanary.HprofGraph
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+import kotlin.reflect.KClass
+
+class JvmHprofParsingTest {
+
+  @get:Rule
+  var testFolder = TemporaryFolder()
+
+  @Test fun dumpHeapAndReadString() {
+    val hprofFolder = testFolder.newFolder()
+    val hprofFile = File(hprofFolder, "jvm_heap.hprof")
+
+    JvmTestHeapDumper.dumpHeap(hprofFile.absolutePath)
+
+    val (graph, closeable) = HprofGraph.readHprof(hprofFile)
+
+    closeable.use {
+      val testInstances = graph.instanceSequence()
+          .filter { it.className == JvmHprofParsingTest::class.name }
+          .toList()
+
+      assertThat(testInstances).hasSize(1)
+      val test = testInstances[0]
+      val folderPath = test[JvmHprofParsingTest::class.name, "testFolder"]!!
+          .valueAsInstance!![TemporaryFolder::class.name, "folder"]!!
+          .valueAsInstance!![File::class.name, "path"]!!
+          .value.readAsJavaString()!!
+
+      assertThat(folderPath).isEqualTo(testFolder.root.path)
+    }
+  }
+}
+
+private val KClass<out Any>.name: String
+  get() = this.java.name
--- leakcanary-haha/src/main/java/leakcanary/GraphField.kt
+++ leakcanary-haha/src/main/java/leakcanary/GraphField.kt
@@ -1,9 +1,25 @@
 package leakcanary
 
 import leakcanary.GraphObjectRecord.GraphClassRecord
+import leakcanary.GraphObjectRecord.GraphInstanceRecord
+import leakcanary.GraphObjectRecord.GraphObjectArrayRecord
+import leakcanary.GraphObjectRecord.GraphPrimitiveArrayRecord
 
 class GraphField(
   val classRecord: GraphClassRecord,
   val name: String,
   val value: GraphHeapValue
-)
+) {
+
+  val valueAsClass: GraphClassRecord?
+    get() = value.asObject?.asClass
+
+  val valueAsInstance: GraphInstanceRecord?
+    get() = value.asObject?.asInstance
+
+  val valueAsObjectArray: GraphObjectArrayRecord?
+    get() = value.asObject?.asObjectArray
+
+  val valueAsPrimitiveArray: GraphPrimitiveArrayRecord?
+    get() = value.asObject?.asPrimitiveArray
+}
