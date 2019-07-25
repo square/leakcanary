95e1add4c982ba9fb9328c1d45ffdf985423c791
Pierre-Yves Ricau <py@squareup.com>
2019-07-25 09:03:09 -0700

    [Do Not Merge] Demonstrating analyzing heap in release builds

--- leakcanary-android-sample/src/main/java/com/example/leakcanary/ExampleApplication.kt
+++ leakcanary-android-sample/src/main/java/com/example/leakcanary/ExampleApplication.kt
@@ -16,8 +16,23 @@
 package com.example.leakcanary
 
 import android.app.Application
+import android.os.Debug
 import android.os.StrictMode
+import android.os.SystemClock
 import android.view.View
+import leakcanary.AppWatcher
+import leakcanary.GcTrigger
+import leakcanary.KeyedWeakReference
+import leakcanary.OnObjectRetainedListener
+import shark.AndroidObjectInspectors
+import shark.AndroidReferenceMatchers
+import shark.HeapAnalyzer
+import shark.OnAnalysisProgressListener
+import shark.SharkLog
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 open class ExampleApplication : Application() {
   val leakedViews = mutableListOf<View>()
@@ -25,6 +40,60 @@ open class ExampleApplication : Application() {
   override fun onCreate() {
     super.onCreate()
     enabledStrictMode()
+    if (!BuildConfig.DEBUG) {
+      watchForLeaksInReleaseBuilds()
+    }
+  }
+
+  private fun watchForLeaksInReleaseBuilds() {
+    AppWatcher.config = AppWatcher.config.copy(enabled = true)
+    AppWatcher.objectWatcher.addOnObjectRetainedListener(OnObjectRetainedListener {
+      SharkLog.d("Received object retained")
+      GcTrigger.Default.runGc()
+      if (AppWatcher.objectWatcher.retainedObjectCount == 0) {
+        return@OnObjectRetainedListener
+      }
+      SharkLog.d("Dumping the heap")
+      val heapDumpUptimeMillis = SystemClock.uptimeMillis()
+      val directory = File(filesDir, "leakcanary")
+      val success = (directory.mkdirs() || directory.exists()) && directory.canWrite()
+      if (!success) {
+        SharkLog.d(
+            "Could not create heap dump directory in app storage: [%s]", directory.absolutePath
+        )
+        return@OnObjectRetainedListener
+      }
+      val fileName = SimpleDateFormat("yyyy-MM-dd_HH-mm-ss_SSS'.hprof'", Locale.US).format(
+          Date()
+      )
+      val heapDumpFile = File(directory, fileName)
+
+      KeyedWeakReference.heapDumpUptimeMillis = heapDumpUptimeMillis
+      try {
+        Debug.dumpHprofData(heapDumpFile.absolutePath)
+        if (heapDumpFile.length() == 0L) {
+          SharkLog.d("Dumped heap file is 0 byte length")
+          return@OnObjectRetainedListener
+        }
+      } catch (e: Exception) {
+        SharkLog.d(e, "Could not dump heap")
+        // Abort heap dump
+        return@OnObjectRetainedListener
+      }
+
+      if (!heapDumpFile.exists()) {
+        SharkLog.d("Hprof file missing $heapDumpFile")
+        return@OnObjectRetainedListener
+      }
+      val heapAnalyzer = HeapAnalyzer(OnAnalysisProgressListener.NO_OP)
+
+      val heapAnalysis =
+        heapAnalyzer.checkForLeaks(
+            heapDumpFile, AndroidReferenceMatchers.appDefaults, true,
+            AndroidObjectInspectors.appDefaults
+        )
+      SharkLog.d(heapAnalysis.toString())
+    })
   }
 
   private fun enabledStrictMode() {
