e48acce6c383eddf2758cab4e7613e6f572177eb
PY <pyricau@users.noreply.github.com>
2019-07-29 17:01:24 -0700

    API to show / hide leak launcher icon (#1509)
    See #1502

--- leakcanary-android-core/src/main/java/leakcanary/LeakCanary.kt
+++ leakcanary-android-core/src/main/java/leakcanary/LeakCanary.kt
@@ -152,6 +152,24 @@ object LeakCanary {
   fun newLeakDisplayActivityIntent() = InternalLeakCanary.leakDisplayActivityIntent
 
   /**
+   * Dynamically shows / hides the launcher icon for the leak display activity.
+   * Note: you can change the default value by overriding the leak_canary_add_launcher_icon
+   * boolean resource:
+   *
+   * ```
+   * <?xml version="1.0" encoding="utf-8"?>
+   * <resources>
+   *   <bool name="leak_canary_add_launcher_icon">false</bool>
+   * </resources>
+   * ```
+   */
+  fun showLeakDisplayActivityLauncherIcon(showLauncherIcon: Boolean) {
+    InternalLeakCanary.setEnabledBlocking(
+        "leakcanary.internal.activity.LeakLauncherActivity", showLauncherIcon
+    )
+  }
+
+  /**
    * Immediately triggers a heap dump and analysis, if there is at least one retained instance
    * tracked by [AppWatcher.objectWatcher]. If there are no retained instances then the heap will not
    * be dumped and a notification will be shown instead.
