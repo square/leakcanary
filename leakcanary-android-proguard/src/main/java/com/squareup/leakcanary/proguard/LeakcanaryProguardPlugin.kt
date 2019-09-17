4794a17b806aa7350d2ce6e9d168024d30fd78dd
Michal Zielinski <michal.zielinski@polidea.com>
2019-09-17 13:56:38 +0200

    Gradle plugin that puts proguard mapping file in the apk as an asset

---
+++ leakcanary-android-proguard/src/main/java/com/squareup/leakcanary/proguard/LeakcanaryProguardPlugin.kt
@@ -0,0 +1,53 @@
+package com.squareup.leakcanary.proguard
+
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LeakcanaryProguardPlugin : Plugin<Project> {
+  override fun apply(project: Project) {
+    val extension = project.extensions.create("leakCanary", LeakCanaryProguardExtension::class.java)
+
+    val androidPluginHandler = { _: Plugin<*> ->
+      val android = project.extensions.getByType(AppExtension::class.java)
+
+      project.afterEvaluate {
+        android.applicationVariants.all { variant ->
+
+          if (extension.obfuscatedVariantNames?.contains(variant.name) == true) {
+            val copyProguardMappingFileTask = project.tasks.create(
+                "leakCanaryCopyProguardMappingFor${variant.name.capitalize()}",
+                CopyProguardMappingFileTask::class.java
+            ) {
+              it.variantDirName = variant.dirName
+              it.mappingFile = variant.mappingFile
+            }
+
+            val mappingGeneratingTask = project.tasks.find { task ->
+              task.name.contains("transformClassesAndResourcesWithR8", true)
+            } ?: project.tasks.find { task ->
+              task.name.contains("transformClassesAndResourcesWithProguard", true)
+            }
+
+            mappingGeneratingTask?.let {
+              mappingGeneratingTask.finalizedBy(copyProguardMappingFileTask)
+
+              variant.mergeAssetsProvider?.configure { provider ->
+                provider.dependsOn(copyProguardMappingFileTask)
+              }
+
+              android.sourceSets
+                  .find { it.name == variant.name }
+                  ?.apply {
+                    assets.setSrcDirs(
+                        assets.srcDirs + copyProguardMappingFileTask.leakCanaryAssetsOutputDirectory
+                    )
+                  }
+            }
+          }
+        }
+      }
+    }
+    project.plugins.withId("com.android.application", androidPluginHandler)
+  }
+}
