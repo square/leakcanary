ce2d1e2005d0fa630b3c748ed476eee81fafdbf0
Michal Zielinski <michal.zielinski@polidea.com>
2019-10-06 17:26:01 +0200

    Support library modules and renamed to avoid "proguard" name

--- leakcanary-android-proguard/src/main/java/com/squareup/leakcanary/proguard/LeakCanaryProguardExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.squareup.leakcanary.proguard
-
-open class LeakCanaryProguardExtension {
-  var obfuscatedVariantNames: Collection<String>? = null
-
-  fun obfuscatedVariantNames(vararg names: String) {
-    obfuscatedVariantNames = names.toList()
-  }
-}
