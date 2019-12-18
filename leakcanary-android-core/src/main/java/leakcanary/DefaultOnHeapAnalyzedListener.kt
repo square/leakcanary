8c962a59852a7ae43be1920c2c875fe842c7561c
PY <pyricau@users.noreply.github.com>
2019-12-18 23:57:37 +0100

    Updated result notification (#1688)
    Now displayed the count of retained objects and distinct leaks
    
    Fixes #1686

--- leakcanary-android-core/src/main/java/leakcanary/DefaultOnHeapAnalyzedListener.kt
+++ leakcanary-android-core/src/main/java/leakcanary/DefaultOnHeapAnalyzedListener.kt
@@ -10,8 +10,8 @@ import leakcanary.internal.activity.db.HeapAnalysisTable
 import leakcanary.internal.activity.db.LeakTable
 import leakcanary.internal.activity.db.LeaksDbHelper
 import leakcanary.internal.activity.screen.HeapAnalysisFailureScreen
-import leakcanary.internal.activity.screen.HeapDumpsScreen
 import leakcanary.internal.activity.screen.HeapDumpScreen
+import leakcanary.internal.activity.screen.HeapDumpsScreen
 import shark.HeapAnalysis
 import shark.HeapAnalysisFailure
 import shark.HeapAnalysisSuccess
@@ -37,23 +37,10 @@ class DefaultOnHeapAnalyzedListener(private val application: Application) : OnHe
           R.string.leak_canary_analysis_failed
       ) to HeapAnalysisFailureScreen(id)
       is HeapAnalysisSuccess -> {
-        var leakCount = 0
-        var newLeakCount = 0
-        var knownLeakCount = 0
-        var libraryLeakCount = 0
-
-        for ((_, projection) in groupProjections) {
-          leakCount += projection.leakCount
-          when {
-            projection.isLibraryLeak -> libraryLeakCount += projection.leakCount
-            projection.isNew -> newLeakCount += projection.leakCount
-            else -> knownLeakCount += projection.leakCount
-          }
-        }
-
+        val retainedObjectCount = groupProjections.values.sumBy { it.leakCount }
+        val leakTypeCount = groupProjections.size
         application.getString(
-            R.string.leak_canary_analysis_success_notification, leakCount, newLeakCount,
-            knownLeakCount, libraryLeakCount
+            R.string.leak_canary_analysis_success_notification, retainedObjectCount, leakTypeCount
         ) to HeapDumpScreen(id)
       }
     }
