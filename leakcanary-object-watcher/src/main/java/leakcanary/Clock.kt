ea4abf77b756f3e10f40d4c35b049ca8a81d0bee
Pierre-Yves Ricau <py@squareup.com>
2019-12-23 23:26:19 +0100

    Add recipe for LeakCanary on JVM
    Fixes #54

--- leakcanary-object-watcher/src/main/java/leakcanary/Clock.kt
+++ leakcanary-object-watcher/src/main/java/leakcanary/Clock.kt
@@ -8,4 +8,23 @@ interface Clock {
    * On Android VMs, this should return android.os.SystemClock.uptimeMillis().
    */
   fun uptimeMillis(): Long
+
+  companion object {
+    /**
+     * Utility function to create a [Clock] from the passed in [block] lambda
+     * instead of using the anonymous `object : Clock` syntax.
+     *
+     * Usage:
+     *
+     * ```kotlin
+     * val clock = Clock {
+     *
+     * }
+     * ```
+     */
+    inline operator fun invoke(crossinline block: () -> Long): Clock =
+      object : Clock {
+        override fun uptimeMillis(): Long = block()
+      }
+  }
 }
