58c856dcab2b93d92d3a89839de4eb2adc25ed17
Artem Chubaryan <artem@squareup.com>
2020-04-28 10:22:12 -0500

    Added unit tests for LeakCanary.Config.Builder and AppWatcher.Config.Builder to make sure their APIs match the data classes (#1802)

--- leakcanary-android-core/src/test/java/leakcanary/LeakCanaryConfigTest.kt
+++ leakcanary-android-core/src/test/java/leakcanary/LeakCanaryConfigTest.kt
@@ -5,28 +5,22 @@ import org.junit.Test
 import kotlin.reflect.full.memberFunctions
 import kotlin.reflect.full.memberProperties
 
-class AppWatcherTest {
-
-  @Test fun appWatcherLoads_notInstalled() {
-    assertThat(AppWatcher.isInstalled)
-        .describedAs("Ensure AppWatcher doesn't crash in JUnit tests")
-        .isFalse()
-  }
+class LeakCanaryConfigTest {
 
   /**
-   * Validates that each field in [AppWatcher.Config] has a matching builder function
-   * in [AppWatcher.Config.Builder]
+   * Validates that each field in [LeakCanary.Config] has a matching builder function
+   * in [LeakCanary.Config.Builder]
    */
-  @Test fun `AppWatcher Config Builder matches AppWatcher Config`() {
+  @Test fun `LeakCanary Config Builder matches LeakCanary Config`() {
     assertThat(configProperties())
         .containsExactlyInAnyOrderElementsOf(configBuilderFunctions())
   }
 
-  private fun configBuilderFunctions() = AppWatcher.Config.Builder::class.memberFunctions
+  private fun configBuilderFunctions() = LeakCanary.Config.Builder::class.memberFunctions
       .map { it.name }
       .subtract(listOf("build", "equals", "hashCode", "toString"))
 
-  private fun configProperties() = AppWatcher.Config::class.memberProperties
+  private fun configProperties() = LeakCanary.Config::class.memberProperties
       .filter { member ->
         // Ignore deprecated fields, we don't need builders for those
         member.annotations.none { it is Deprecated }
