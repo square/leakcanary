389eae1b89c423bc89d8a657e0a9e643fea20773
CkEsc <iamckesc@gmail.com>
2020-05-13 18:58:06 +0300

    Add workaround for Activity$1#this$0 leak.
    Fix taken from https://twitter.com/Piwai/status/1169274624749658112

--- shark-android/src/main/java/shark/AndroidReferenceMatchers.kt
+++ shark-android/src/main/java/shark/AndroidReferenceMatchers.kt
@@ -60,7 +60,11 @@ enum class AndroidReferenceMatchers {
                   "anonymous subclass. That anonymous subclass has a reference to the activity. " +
                   "Another process is keeping the android.app.IRequestFinishCallback\$Stub " +
                   "reference alive long after Activity.onDestroyed() has been called, " +
-                  "causing the activity to leak."
+                  "causing the activity to leak." +
+                  "Fix: You can \"fix\" this leak by overriding Activity.onBackPressed() and calling " +
+                  "Activity.finishAfterTransition(); instead of super if the activity is task root and the " +
+                  "fragment stack is empty." +
+                  "Tracked here: https://issuetracker.google.com/issues/139738913"
           ) {
               sdkInt == 29
           }
