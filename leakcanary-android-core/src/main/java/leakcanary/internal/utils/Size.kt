465c59a3a025706fd4ee6f9d42ea9350f8aaaf17
Olivier Patry <olivier.patry@gmail.com>
2020-10-05 20:19:36 +0200

    Use kotlin.math.pow and kotlin.math.ln in humanReadableByteCount

--- leakcanary-android-core/src/main/java/leakcanary/internal/utils/Size.kt
+++ leakcanary-android-core/src/main/java/leakcanary/internal/utils/Size.kt
@@ -1,5 +1,8 @@
 package leakcanary.internal.utils
 
+import kotlin.math.ln
+import kotlin.math.pow
+
 
 // https://stackoverflow.com/a/3758880
 internal fun humanReadableByteCount(
@@ -8,7 +11,7 @@ internal fun humanReadableByteCount(
 ): String {
     val unit = if (si) 1000 else 1024
     if (bytes < unit) return "$bytes B"
-    val exp = (Math.log(bytes.toDouble()) / Math.log(unit.toDouble())).toInt()
+    val exp = (ln(bytes.toDouble()) / ln(unit.toDouble())).toInt()
     val pre = (if (si) "kMGTPE" else "KMGTPE")[exp - 1] + if (si) "" else "i"
-    return String.format("%.1f %sB", bytes / Math.pow(unit.toDouble(), exp.toDouble()), pre)
+    return String.format("%.1f %sB", bytes / unit.toDouble().pow(exp.toDouble()), pre)
 }
--- shark/src/main/java/shark/LeakTrace.kt
+++ shark/src/main/java/shark/LeakTrace.kt
@@ -7,6 +7,8 @@ import shark.LeakTraceObject.LeakingStatus.UNKNOWN
 import shark.LeakTraceReference.ReferenceType.STATIC_FIELD
 import shark.internal.createSHA1Hash
 import java.io.Serializable
+import kotlin.math.ln
+import kotlin.math.pow
 
 /**
  * The best strong reference path from a GC root to the leaking object. "Best" here means the
@@ -103,9 +105,9 @@ data class LeakTrace(
   ): String {
     val unit = if (si) 1000 else 1024
     if (bytes < unit) return "$bytes B"
-    val exp = (Math.log(bytes.toDouble()) / Math.log(unit.toDouble())).toInt()
+    val exp = (ln(bytes.toDouble()) / ln(unit.toDouble())).toInt()
     val pre = (if (si) "kMGTPE" else "KMGTPE")[exp - 1] + if (si) "" else "i"
-    return String.format("%.1f %sB", bytes / Math.pow(unit.toDouble(), exp.toDouble()), pre)
+    return String.format("%.1f %sB", bytes / unit.toDouble().pow(exp.toDouble()), pre)
   }
 
   private fun leakTraceAsString(showLeakingStatus: Boolean): String {
