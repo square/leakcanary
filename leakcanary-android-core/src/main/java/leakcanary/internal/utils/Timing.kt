f93ff98a5578f255cae052ef592c796d05d8a8f4
Pierre-Yves Ricau <py@squareup.com>
2020-10-14 16:43:36 -0700

    Make internal API internal

--- leakcanary-android-core/src/main/java/leakcanary/internal/utils/Timing.kt
+++ leakcanary-android-core/src/main/java/leakcanary/internal/utils/Timing.kt
@@ -5,7 +5,7 @@ import android.os.SystemClock
 /**
  * Executes the given [block] and returns elapsed time in milliseconds using [SystemClock.uptimeMillis]
  */
-inline fun measureDurationMillis(block: () -> Unit): Long {
+internal inline fun measureDurationMillis(block: () -> Unit): Long {
     val start = SystemClock.uptimeMillis()
     block()
     return SystemClock.uptimeMillis() - start
