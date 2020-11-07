e649f36f87e95e0c5ecdfe848d444c9a638ad0dd
Olivier Patry <olivier.patry@gmail.com>
2020-11-07 22:23:33 +0100

    Protect against `NoSuchFieldException` when looking for `orchestratorListener` field (fixes #1986)
    Such field might be missing, observed with `android-test 1.3.1-alpha02` at least.

--- leakcanary-android-instrumentation/src/main/java/leakcanary/internal/TestResultPublisher.kt
+++ leakcanary-android-instrumentation/src/main/java/leakcanary/internal/TestResultPublisher.kt
@@ -19,11 +19,18 @@ internal interface TestResultPublisher {
     fun install(): TestResultPublisher {
       val instrumentation = InstrumentationRegistry.getInstrumentation()
       val orchestratorListener = if (instrumentation is AndroidJUnitRunner) {
-        AndroidJUnitRunner::class.java.getDeclaredField("orchestratorListener")
+        try {
+          AndroidJUnitRunner::class.java.getDeclaredField("orchestratorListener")
             .run {
               isAccessible = true
               get(instrumentation) as OrchestratedInstrumentationListener?
             }
+        } catch (e: NoSuchFieldException) {
+          // Starting from `android-test 1.3.1-alpha02`, test failure aren't properly reported
+          // due to missing `orchestratorListener` field. Avoid failure for now.
+          // See https://github.com/square/leakcanary/issues/1986
+          null
+        }
       } else null
       return if (orchestratorListener != null) {
         SharkLog.d { "Android Test Orchestrator detected, failures will be sent via binder callback" }
