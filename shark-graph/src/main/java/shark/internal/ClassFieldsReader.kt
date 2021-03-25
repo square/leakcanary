1e23b7a5675b1a41e1c5849c1015d0bc71872ac1
Pierre-Yves Ricau <py@squareup.com>
2021-03-25 11:58:54 -0700

    Fix thread safety crash
    This allows position to be thread local.
    
    Fixes #2084

--- shark-graph/src/main/java/shark/internal/ClassFieldsReader.kt
+++ shark-graph/src/main/java/shark/internal/ClassFieldsReader.kt
@@ -28,143 +28,160 @@ internal class ClassFieldsReader(
   private val classFieldBytes: ByteArray
 ) {
 
-  private var position = 0
-
   fun classDumpStaticFields(indexedClass: IndexedClass): List<StaticFieldRecord> {
-    position = indexedClass.fieldsIndex
-    val staticFieldCount = readUnsignedShort()
-    val staticFields = ArrayList<StaticFieldRecord>(staticFieldCount)
-    for (i in 0 until staticFieldCount) {
-      val nameStringId = readId()
-      val type = readUnsignedByte()
-      val value = readValue(type)
-      staticFields.add(
-        StaticFieldRecord(
-          nameStringId = nameStringId,
-          type = type,
-          value = value
+    return read(initialPosition = indexedClass.fieldsIndex) {
+      val staticFieldCount = readUnsignedShort()
+      val staticFields = ArrayList<StaticFieldRecord>(staticFieldCount)
+      for (i in 0 until staticFieldCount) {
+        val nameStringId = readId()
+        val type = readUnsignedByte()
+        val value = readValue(type)
+        staticFields.add(
+          StaticFieldRecord(
+            nameStringId = nameStringId,
+            type = type,
+            value = value
+          )
         )
-      )
+      }
+      staticFields
     }
-    return staticFields
   }
 
   fun classDumpFields(indexedClass: IndexedClass): List<FieldRecord> {
-    position = indexedClass.fieldsIndex
+    return read(initialPosition = indexedClass.fieldsIndex) {
+      skipStaticFields()
 
-    skipStaticFields()
-
-    val fieldCount = readUnsignedShort()
-    val fields = ArrayList<FieldRecord>(fieldCount)
-    for (i in 0 until fieldCount) {
-      fields.add(FieldRecord(nameStringId = readId(), type = readUnsignedByte()))
+      val fieldCount = readUnsignedShort()
+      val fields = ArrayList<FieldRecord>(fieldCount)
+      for (i in 0 until fieldCount) {
+        fields.add(FieldRecord(nameStringId = readId(), type = readUnsignedByte()))
+      }
+      fields
     }
-    return fields
   }
 
   fun classDumpHasReferenceFields(indexedClass: IndexedClass): Boolean {
-    position = indexedClass.fieldsIndex
-    skipStaticFields()
-    val fieldCount = readUnsignedShort()
-    for (i in 0 until fieldCount) {
-      position += identifierByteSize
-      val type = readUnsignedByte()
-      if (type == PrimitiveType.REFERENCE_HPROF_TYPE) {
-        return true
+    return read(initialPosition = indexedClass.fieldsIndex) {
+      skipStaticFields()
+      val fieldCount = readUnsignedShort()
+      for (i in 0 until fieldCount) {
+        position += identifierByteSize
+        val type = readUnsignedByte()
+        if (type == PrimitiveType.REFERENCE_HPROF_TYPE) {
+          return@read true
+        }
+      }
+      return@read false
+    }
+  }
+
+  private val readInFlightThreadLocal = object : ThreadLocal<ReadInFlight>() {
+    override fun initialValue() = ReadInFlight()
+  }
+
+  private fun <R> read(
+    initialPosition: Int,
+    block: ReadInFlight.() -> R
+  ): R {
+    val readInFlight = readInFlightThreadLocal.get()
+    readInFlight.position = initialPosition
+    return readInFlight.run(block)
+  }
+
+  private inner class ReadInFlight {
+    var position = 0
+
+    fun skipStaticFields() {
+      val staticFieldCount = readUnsignedShort()
+      for (i in 0 until staticFieldCount) {
+        position += identifierByteSize
+        val type = readUnsignedByte()
+        position += if (type == PrimitiveType.REFERENCE_HPROF_TYPE) {
+          identifierByteSize
+        } else {
+          PrimitiveType.byteSizeByHprofType.getValue(type)
+        }
       }
     }
-    return false
-  }
 
-  private fun skipStaticFields() {
-    val staticFieldCount = readUnsignedShort()
-    for (i in 0 until staticFieldCount) {
-      position += identifierByteSize
-      val type = readUnsignedByte()
-      position += if (type == PrimitiveType.REFERENCE_HPROF_TYPE) {
-        identifierByteSize
-      } else {
-        PrimitiveType.byteSizeByHprofType.getValue(type)
+    fun readValue(type: Int): ValueHolder {
+      return when (type) {
+        PrimitiveType.REFERENCE_HPROF_TYPE -> ReferenceHolder(readId())
+        BOOLEAN_TYPE -> BooleanHolder(readBoolean())
+        CHAR_TYPE -> CharHolder(readChar())
+        FLOAT_TYPE -> FloatHolder(readFloat())
+        DOUBLE_TYPE -> DoubleHolder(readDouble())
+        BYTE_TYPE -> ByteHolder(readByte())
+        SHORT_TYPE -> ShortHolder(readShort())
+        INT_TYPE -> IntHolder(readInt())
+        LONG_TYPE -> LongHolder(readLong())
+        else -> throw IllegalStateException("Unknown type $type")
       }
     }
-  }
 
-  private fun readValue(type: Int): ValueHolder {
-    return when (type) {
-      PrimitiveType.REFERENCE_HPROF_TYPE -> ReferenceHolder(readId())
-      BOOLEAN_TYPE -> BooleanHolder(readBoolean())
-      CHAR_TYPE -> CharHolder(readChar())
-      FLOAT_TYPE -> FloatHolder(readFloat())
-      DOUBLE_TYPE -> DoubleHolder(readDouble())
-      BYTE_TYPE -> ByteHolder(readByte())
-      SHORT_TYPE -> ShortHolder(readShort())
-      INT_TYPE -> IntHolder(readInt())
-      LONG_TYPE -> LongHolder(readLong())
-      else -> throw IllegalStateException("Unknown type $type")
+    fun readByte(): Byte {
+      return classFieldBytes[position++]
     }
-  }
 
-  private fun readByte(): Byte {
-    return classFieldBytes[position++]
-  }
-
-  private fun readInt(): Int {
-    return (classFieldBytes[position++].toInt() and 0xff shl 24) or
-      (classFieldBytes[position++].toInt() and 0xff shl 16) or
-      (classFieldBytes[position++].toInt() and 0xff shl 8) or
-      (classFieldBytes[position++].toInt() and 0xff)
-  }
-
-  private fun readLong(): Long {
-    return (classFieldBytes[position++].toLong() and 0xff shl 56) or
-      (classFieldBytes[position++].toLong() and 0xff shl 48) or
-      (classFieldBytes[position++].toLong() and 0xff shl 40) or
-      (classFieldBytes[position++].toLong() and 0xff shl 32) or
-      (classFieldBytes[position++].toLong() and 0xff shl 24) or
-      (classFieldBytes[position++].toLong() and 0xff shl 16) or
-      (classFieldBytes[position++].toLong() and 0xff shl 8) or
-      (classFieldBytes[position++].toLong() and 0xff)
-  }
-
-  private fun readShort(): Short {
-    return ((classFieldBytes[position++].toInt() and 0xff shl 8) or
-      (classFieldBytes[position++].toInt() and 0xff)).toShort()
-  }
-
-  private fun readUnsignedShort(): Int {
-    return readShort().toInt() and 0xFFFF
-  }
-
-  private fun readUnsignedByte(): Int {
-    return readByte().toInt() and 0xFF
-  }
-
-  private fun readId(): Long {
-    // As long as we don't interpret IDs, reading signed values here is fine.
-    return when (identifierByteSize) {
-      1 -> readByte().toLong()
-      2 -> readShort().toLong()
-      4 -> readInt().toLong()
-      8 -> readLong()
-      else -> throw IllegalArgumentException("ID Length must be 1, 2, 4, or 8")
+    fun readInt(): Int {
+      return (classFieldBytes[position++].toInt() and 0xff shl 24) or
+        (classFieldBytes[position++].toInt() and 0xff shl 16) or
+        (classFieldBytes[position++].toInt() and 0xff shl 8) or
+        (classFieldBytes[position++].toInt() and 0xff)
     }
-  }
 
-  private fun readBoolean(): Boolean {
-    return readByte()
-      .toInt() != 0
-  }
+    fun readLong(): Long {
+      return (classFieldBytes[position++].toLong() and 0xff shl 56) or
+        (classFieldBytes[position++].toLong() and 0xff shl 48) or
+        (classFieldBytes[position++].toLong() and 0xff shl 40) or
+        (classFieldBytes[position++].toLong() and 0xff shl 32) or
+        (classFieldBytes[position++].toLong() and 0xff shl 24) or
+        (classFieldBytes[position++].toLong() and 0xff shl 16) or
+        (classFieldBytes[position++].toLong() and 0xff shl 8) or
+        (classFieldBytes[position++].toLong() and 0xff)
+    }
 
-  private fun readChar(): Char {
-    return readShort().toChar()
-  }
+    fun readShort(): Short {
+      return ((classFieldBytes[position++].toInt() and 0xff shl 8) or
+        (classFieldBytes[position++].toInt() and 0xff)).toShort()
+    }
 
-  private fun readFloat(): Float {
-    return Float.fromBits(readInt())
-  }
+    fun readUnsignedShort(): Int {
+      return readShort().toInt() and 0xFFFF
+    }
 
-  private fun readDouble(): Double {
-    return Double.fromBits(readLong())
+    fun readUnsignedByte(): Int {
+      return readByte().toInt() and 0xFF
+    }
+
+    fun readId(): Long {
+      // As long as we don't interpret IDs, reading signed values here is fine.
+      return when (identifierByteSize) {
+        1 -> readByte().toLong()
+        2 -> readShort().toLong()
+        4 -> readInt().toLong()
+        8 -> readLong()
+        else -> throw IllegalArgumentException("ID Length must be 1, 2, 4, or 8")
+      }
+    }
+
+    fun readBoolean(): Boolean {
+      return readByte()
+        .toInt() != 0
+    }
+
+    fun readChar(): Char {
+      return readShort().toChar()
+    }
+
+    fun readFloat(): Float {
+      return Float.fromBits(readInt())
+    }
+
+    fun readDouble(): Double {
+      return Double.fromBits(readLong())
+    }
   }
 
   companion object {
