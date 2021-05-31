1e4542e696ddeda98f723bd0ecd6eaaddb46f35a
Summer Li <summer.lyr@outlook.com>
2021-05-31 15:55:59 +0800

    Use encodeUtf8().sha1().hex() instead

--- shark/src/main/java/shark/internal/Strings.kt
+++ shark/src/main/java/shark/internal/Strings.kt
@@ -1,9 +1,7 @@
 package shark.internal
 
-import java.math.BigInteger
+import okio.ByteString.Companion.encodeUtf8
 import java.nio.charset.Charset
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 @JvmField
 internal val UTF_8: Charset = Charset.forName("UTF-8")
@@ -13,22 +11,7 @@ internal fun String.lastSegment(segmentingChar: Char): String {
   return if (separator == -1) this else this.substring(separator + 1)
 }
 
-internal fun String.createSHA1Hash(): String = createHash(this, "SHA-1")
-
-private fun createHash(
-  text: String,
-  algorithm: String
-): String {
-  try {
-    // Create MD5 Hash.
-    val digest = MessageDigest.getInstance(algorithm)
-    digest.update(text.getBytes())
-
-    return BigInteger(1, digest.digest()).toString(16).padStart(40, '0')
-  } catch (e: NoSuchAlgorithmException) {
-    throw AssertionError("Unable to construct MessageDigest for $algorithm")
-  }
-}
+internal fun String.createSHA1Hash(): String = encodeUtf8().sha1().hex()
 
 /** Gets the string as an array of UTF-8 bytes. */
 internal fun String.getBytes(): ByteArray = toByteArray(UTF_8)
