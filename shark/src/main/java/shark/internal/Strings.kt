abb9c808902e503394facd90b752eb13b0577943
summerli <summer.lyr@outlook.com>
2021-06-05 07:56:12 +0800

    Fix cutting ‘0’ when creating SHA1 hash (#2133)
    * Fix cutting 0s when creating SHA1 hash
    
    * Use encodeUtf8().sha1().hex() instead
    
    * Remove unused fun
    
    * Remove unused field UTF_8

--- shark/src/main/java/shark/internal/Strings.kt
+++ shark/src/main/java/shark/internal/Strings.kt
@@ -1,43 +1,10 @@
 package shark.internal
 
-import java.nio.charset.Charset
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
-
-@JvmField
-internal val UTF_8: Charset = Charset.forName("UTF-8")
+import okio.ByteString.Companion.encodeUtf8
 
 internal fun String.lastSegment(segmentingChar: Char): String {
   val separator = lastIndexOf(segmentingChar)
   return if (separator == -1) this else this.substring(separator + 1)
 }
 
-internal fun String.createSHA1Hash(): String = createHash(this, "SHA-1")
-
-/**
- * Derived from
- * [this snippet](http://www.androidsnippets.com/create-a-md5-hash-and-dump-as-a-hex-string).
- */
-private fun createHash(
-  text: String,
-  algorithm: String
-): String {
-  try {
-    // Create MD5 Hash.
-    val digest = MessageDigest.getInstance(algorithm)
-    digest.update(text.getBytes())
-    val messageDigest = digest.digest()
-
-    // Create Hex String.
-    val hexString = StringBuilder()
-    for (b in messageDigest) {
-      hexString.append(Integer.toHexString(0xff and b.toInt()))
-    }
-    return hexString.toString()
-  } catch (e: NoSuchAlgorithmException) {
-    throw AssertionError("Unable to construct MessageDigest for $algorithm")
-  }
-}
-
-/** Gets the string as an array of UTF-8 bytes. */
-internal fun String.getBytes(): ByteArray = toByteArray(UTF_8)
+internal fun String.createSHA1Hash(): String = encodeUtf8().sha1().hex()
