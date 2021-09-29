63ef72ed11b7b5825420eeaf6e2f5454c38e72cf
Wyko Rijnsburger <utwyko@gmail.com>
2021-09-29 11:44:04 +0200

    Fix crash when showing toast on TV devices on API level 30.
    Custom toast views are deprecated from API level 30 on (see [docs](https://developer.android.com/reference/android/widget/Toast#getView())). A custom icon can probably still be shown using a Snackbar when app is in the foreground.

--- leakcanary-android-core/src/main/java/leakcanary/internal/tv/TvToast.kt
+++ leakcanary-android-core/src/main/java/leakcanary/internal/tv/TvToast.kt
@@ -3,12 +3,16 @@ package leakcanary.internal.tv
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.os.Build
 import android.widget.TextView
 import android.widget.Toast
 import com.squareup.leakcanary.core.R
 
 /**
  * Toast helper for Android TV preconfigured with LeakCanary icon.
+ *
+ * The icon is only shown on API level 29 and below, as custom toast views are deprecated from API level 30 on (see
+ * [docs](https://developer.android.com/reference/android/widget/Toast#getView()))
  */
 internal object TvToast {
 
@@ -25,10 +29,16 @@ internal object TvToast {
     text: CharSequence
   ): Toast {
     val toast: Toast = Toast.makeText(activity, text, Toast.LENGTH_LONG)
-    val textView = toast.view.findViewById<TextView>(android.R.id.message)
-    textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.leak_canary_icon, 0, 0, 0)
-    textView.compoundDrawablePadding =
-      activity.resources.getDimensionPixelSize(R.dimen.leak_canary_toast_icon_tv_padding)
+
+    // Custom toast views are deprecated from API level 30 on, see
+    // https://developer.android.com/reference/android/widget/Toast#getView()
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+      val textView = toast.view.findViewById<TextView>(android.R.id.message)
+      textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.leak_canary_icon, 0, 0, 0)
+      textView.compoundDrawablePadding =
+        activity.resources.getDimensionPixelSize(R.dimen.leak_canary_toast_icon_tv_padding)
+    }
+
     return toast
   }
 }
