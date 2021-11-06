1257b4d667f6594eda9fe8eebc3a47bde1244fc8
itznotabug <itznotabug@gmail.com>
2021-11-06 13:04:35 +0530

    Configure process to look for enqueued Work instead of Service.

--- leakcanary-android-process/src/main/java/leakcanary/LeakCanaryProcess.kt
+++ leakcanary-android-process/src/main/java/leakcanary/LeakCanaryProcess.kt
@@ -1,13 +1,8 @@
 package leakcanary
 
-import android.app.ActivityManager
-import android.app.Service
-import android.content.ComponentName
 import android.content.Context
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import android.content.pm.ServiceInfo
-import shark.SharkLog
+import androidx.work.WorkInfo.State
+import androidx.work.WorkManager
 
 /**
  * Used to determine whether the current process is the LeakCanary analyzer process. By depending
@@ -26,84 +21,30 @@ object LeakCanaryProcess {
    * a different process than the normal app process.
    */
   fun isInAnalyzerProcess(context: Context): Boolean {
-    val analyzerServiceClass: Class<out Service>
-    @Suppress("UNCHECKED_CAST")
-    try {
-      analyzerServiceClass =
-        Class.forName("leakcanary.internal.analyzer.service.HeapAnalyzerService") as Class<out Service>
-    } catch (e: Exception) {
-      return false
-    }
-
     var isInAnalyzerProcess: Boolean? = isInAnalyzerProcess
     // This only needs to be computed once per process.
     if (isInAnalyzerProcess == null) {
-      isInAnalyzerProcess = isInServiceProcess(context, analyzerServiceClass)
+      isInAnalyzerProcess = isInWorkerProcess(context)
       this.isInAnalyzerProcess = isInAnalyzerProcess
     }
     return isInAnalyzerProcess
   }
 
-  @Suppress("ReturnCount")
-  private fun isInServiceProcess(
-    context: Context,
-    serviceClass: Class<out Service>
+  private fun isInWorkerProcess(
+    context: Context
   ): Boolean {
-    val packageManager = context.packageManager
-    val packageInfo: PackageInfo
-    try {
-      packageInfo = packageManager.getPackageInfo(context.packageName, PackageManager.GET_SERVICES)
-    } catch (e: Exception) {
-      SharkLog.d(e) { "Could not get package info for ${context.packageName}" }
+    var workerRunning = false
+    val workManager = WorkManager.getInstance(context)
+    val activeWorkers = workManager.getWorkInfosByTag("heap_analyzer_worker").get()
+
+    if (activeWorkers.isEmpty()) {
       return false
     }
 
-    val mainProcess = packageInfo.applicationInfo.processName
-
-    val component = ComponentName(context, serviceClass)
-    val serviceInfo: ServiceInfo
-    try {
-      serviceInfo =
-        packageManager.getServiceInfo(component, PackageManager.GET_DISABLED_COMPONENTS)
-    } catch (ignored: PackageManager.NameNotFoundException) {
-      // Service is disabled.
-      return false
+    for (worker in activeWorkers) {
+      workerRunning = worker.state == State.RUNNING || worker.state == State.ENQUEUED
     }
 
-    if (serviceInfo.processName == null) {
-      SharkLog.d { "Did not expect service $serviceClass to have a null process name" }
-      return false
-    } else if (serviceInfo.processName == mainProcess) {
-      SharkLog.d { "Did not expect service $serviceClass to run in main process $mainProcess" }
-      // Technically we are in the service process, but we're not in the service dedicated process.
-      return false
-    }
-
-    val myPid = android.os.Process.myPid()
-    val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-    var myProcess: ActivityManager.RunningAppProcessInfo? = null
-    val runningProcesses: List<ActivityManager.RunningAppProcessInfo>?
-    try {
-      runningProcesses = activityManager.runningAppProcesses
-    } catch (exception: SecurityException) {
-      // https://github.com/square/leakcanary/issues/948
-      SharkLog.d { "Could not get running app processes $exception" }
-      return false
-    }
-
-    if (runningProcesses != null) {
-      for (process in runningProcesses) {
-        if (process.pid == myPid) {
-          myProcess = process
-          break
-        }
-      }
-    }
-    if (myProcess == null) {
-      SharkLog.d { "Could not find running process for $myPid" }
-      return false
-    }
-
-    return myProcess.processName == serviceInfo.processName
+    return workerRunning
   }
 }
