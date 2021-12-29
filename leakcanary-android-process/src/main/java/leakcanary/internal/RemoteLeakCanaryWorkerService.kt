62b327f7933725d7ee9346308c7a75df3779e722
Pierre-Yves Ricau <py@squareup.com>
2021-12-29 18:06:06 +0100

    Improve WorkManager hacks to be ever cooler

--- leakcanary-android-process/src/main/java/leakcanary/internal/RemoteLeakCanaryWorkerService.kt
+++ leakcanary-android-process/src/main/java/leakcanary/internal/RemoteLeakCanaryWorkerService.kt
@@ -1,8 +1,9 @@
 package leakcanary.internal
 
 import android.app.Application
+import android.content.Context
+import android.content.ContextWrapper
 import androidx.work.Configuration
-import androidx.work.WorkManager
 import androidx.work.multiprocess.RemoteWorkerService
 import leakcanary.AppWatcher
 
@@ -11,6 +12,42 @@ import leakcanary.AppWatcher
  * hosting apps can use RemoteWorkerService without a naming conflict.
  */
 class RemoteLeakCanaryWorkerService : RemoteWorkerService() {
+
+  /**
+   * RemoteLeakCanaryWorkerService is running in the :leakcanary process, and androidx startup only
+   * initializes WorkManager in the main process. In RemoteWorkerService.onCreate(),
+   * WorkManager has not been init, so it would crash. We can't blindly call init() as developers
+   * might be calling WorkManager.initialize() from Application.onCreate() for all processes, in
+   * which case a 2nd init would fail. So we want to init if nothing has init WorkManager before.
+   * But there's no isInit() API. However, WorkManager will automatically pull in the application
+   * context and if that context implements Configuration.Provider then it'll pull the
+   * configuration from it. So we cheat WorkManager by returning a fake app context that provides
+   * our own custom configuration.
+   */
+  class FakeAppContextConfigurationProvider(base: Context) : ContextWrapper(base),
+    Configuration.Provider {
+
+    // No real app context for you, sorry!
+    override fun getApplicationContext() = this
+
+    override fun getWorkManagerConfiguration() = Configuration.Builder()
+      // If the default package name is not set, WorkManager will cancel all runnables
+      // when initialized as it can't tell that it's not running in the main process.
+      // This would lead to an extra round trip where the canceling reaches the main process
+      // which then cancels the remote job and reschedules it and then only the work gets done.
+      .setDefaultProcessName(packageName)
+      .build()
+  }
+
+  private val fakeAppContext by lazy {
+    // We set the base context to the real app context so that getting resources etc still works.
+    FakeAppContextConfigurationProvider(super.getApplicationContext())
+  }
+
+  override fun getApplicationContext(): Context {
+    return fakeAppContext
+  }
+
   override fun onCreate() {
     // Ideally we wouldn't need to install AppWatcher at all here, however
     // the installation triggers InternalsLeakCanary to store the application instance
@@ -23,33 +60,6 @@ class RemoteLeakCanaryWorkerService : RemoteWorkerService() {
         watchersToInstall = emptyList()
       )
     }
-    ensureWorkManagerInit()
     super.onCreate()
-  }
-
-  /**
-   * This is running in the :leakcanary process, and androidx startup only initializes WorkManager
-   * in the main process. At this point, WorkManager has not been init, so super.onCreate() would
-   * crash. We can't blindly call init() as developers might be calling WorkManager.initialize()
-   * from Application.onCreate() for all processes, in which case a 2nd init would fail. So we want
-   * to init if nothing has init WorkManager before. But there's no isInit() API. So we just try
-   * to get an instance, and if that throws we know we need to init.
-   */
-  private fun ensureWorkManagerInit() {
-    val applicationContext = applicationContext
-    try {
-      WorkManager.getInstance(applicationContext)
-    } catch (ignored: Throwable) {
-      WorkManager.initialize(
-        applicationContext,
-        Configuration.Builder()
-          // If the default package name is not set, WorkManager will cancel all runnables
-          // when initialized as it can't tell that it's not running in the main process.
-          // This would lead to an extra round trip where the canceling reaches the main process
-          // which then cancels the remote job and reschedules it and then only the work gets done.
-          .setDefaultProcessName(applicationContext.packageName)
-          .build()
-      )
-    }
   }
 }
