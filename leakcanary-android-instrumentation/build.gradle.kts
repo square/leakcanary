b5ae1a2629e2234eaf9b2dc1e613a8c569525ab6
Goooler <wangzongler@gmail.com>
2021-12-30 17:02:38 +0800

    Convert leakcanary-android-instrumentation's gradle to KTS

---
+++ leakcanary-android-instrumentation/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("com.vanniktech.maven.publish")
+}
+
+android {
+  compileSdk = property("compileSdk").toString().toInt()
+  defaultConfig {
+    targetSdk = property("compileSdk").toString().toInt()
+    minSdk = property("minSdk").toString().toInt()
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  buildFeatures.buildConfig = false
+  lintOptions {
+    disable("GoogleAppIndexingWarning")
+    // junit references java.lang.management
+    ignore("InvalidPackage")
+    checkOnly("Interoperability")
+  }
+}
+
+dependencies {
+  api(project(":leakcanary-android-core"))
+
+  implementation(libs.androidX.test.runner)
+  implementation(libs.kotlin.stdlib)
+
+  androidTestImplementation(libs.androidX.test.core)
+  androidTestImplementation(libs.androidX.test.espresso)
+  androidTestImplementation(libs.androidX.test.rules)
+  androidTestImplementation(libs.androidX.fragment)
+  androidTestImplementation(libs.assertjCore)
+}
