cd74b16a3e890259f58af4ba9e810848b60e82c8
Goooler <wangzongler@gmail.com>
2021-12-30 18:02:25 +0800

    Convert leakcanary-android-deobufuscation's gradle to KTS

---
+++ leakcanary-deobfuscation-gradle-plugin/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+  id("org.jetbrains.kotlin.jvm")
+  id("java-gradle-plugin")
+  id("com.vanniktech.maven.publish")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+gradlePlugin {
+  plugins {
+    register("leakCanary") {
+      id = "com.squareup.leakcanary.deobfuscation"
+      implementationClass =
+        "com.squareup.leakcanary.deobfuscation.LeakCanaryLeakDeobfuscationPlugin"
+    }
+  }
+
+  sourceSets["test"].java.srcDirs("src/test/test-project/src/main/java")
+}
+
+dependencies {
+  implementation(libs.kotlin.stdlib)
+  implementation(libs.kotlinPlugin)
+  implementation(libs.androidPlugin)
+  compileOnly(gradleApi())
+
+  testImplementation(libs.assertjCore)
+  testImplementation(libs.junit)
+}
