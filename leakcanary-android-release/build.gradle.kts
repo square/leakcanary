d9c397e0142efc4e2b90d786dbc8c8ce3e49e0ac
Goooler <wangzongler@gmail.com>
2021-12-30 17:08:31 +0800

    Convert leakcanary-android-release's gradle to KTS

---
+++ leakcanary-android-release/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("com.vanniktech.maven.publish")
+}
+
+android {
+  resourcePrefix = "leak_canary_"
+  compileSdk = property("compileSdk").toString().toInt()
+  defaultConfig {
+    minSdk = 16
+    buildConfigField("String", "LIBRARY_VERSION", "\"${property("VERSION_NAME")}\"")
+    consumerProguardFiles("consumer-proguard-rules.pro")
+  }
+  lintOptions {
+    disable("GoogleAppIndexingWarning")
+    error("ObsoleteSdkInt")
+    checkOnly("Interoperability")
+  }
+}
+
+dependencies {
+  api(project(":shark-android"))
+  api(project(":leakcanary-android-utils"))
+
+  implementation(libs.kotlin.stdlib)
+  implementation(libs.okio2)
+}
