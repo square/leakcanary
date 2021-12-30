6e248dc34cbc8b946dfa6655f2d4c0517164a24a
Goooler <wangzongler@gmail.com>
2021-12-30 16:37:39 +0800

    Convert settings.gradle to KTS

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+include(
+  ":plumber-android",
+  ":leakcanary-android",
+  ":leakcanary-android-core",
+  ":leakcanary-android-instrumentation",
+  ":leakcanary-android-process",
+  ":leakcanary-android-release",
+  ":leakcanary-android-sample",
+  ":leakcanary-android-utils",
+  ":leakcanary-object-watcher",
+  ":leakcanary-object-watcher-android",
+  ":leakcanary-object-watcher-android-androidx",
+  ":leakcanary-object-watcher-android-support-fragments",
+  ":shark",
+  ":shark-android",
+  ":shark-cli",
+  ":shark-graph",
+  ":shark-hprof",
+  ":shark-hprof-test",
+  ":shark-log",
+  ":shark-test",
+  ":leakcanary-deobfuscation-gradle-plugin"
+)
+
+enableFeaturePreview("VERSION_CATALOGS")
