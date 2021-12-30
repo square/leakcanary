ec1a08bd9a6e008c787dd9fd764b0e8abd798deb
Pierre-Yves Ricau <py@squareup.com>
2021-12-30 11:05:48 +0100

    Add AndroidX Startup optional support for Plumber
    Fixes #2178
    
    Co-authored-by: Badalyan Vyacheslav <slavon.net@gmail.com>

---
+++ plumber-android-startup/src/main/java/leakcanary/internal/PlumberStartupInitializer.kt
@@ -0,0 +1,14 @@
+package leakcanary.internal
+
+import android.app.Application
+import android.content.Context
+import androidx.startup.Initializer
+import leakcanary.AndroidLeakFixes
+
+internal class PlumberStartupInitializer : Initializer<PlumberStartupInitializer> {
+  override fun create(context: Context) = apply {
+    val application = context.applicationContext as Application
+    AndroidLeakFixes.applyFixes(application)
+  }
+  override fun dependencies() = emptyList<Class<out Initializer<*>>>()
+}
