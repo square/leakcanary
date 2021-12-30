8d7b734748a8ad1c699c9318743cf0d074c7ace9
Goooler <wangzongler@gmail.com>
2021-12-30 18:06:45 +0800

    Convert leakcanary-object-watcher-android's gradle to KTS

---
+++ leakcanary-object-watcher-android/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("com.vanniktech.maven.publish")
+}
+
+android {
+  resourcePrefix = "leak_canary_watcher_"
+  compileSdk = property("compileSdk").toString().toInt()
+  defaultConfig {
+    minSdk = property("minSdk").toString().toInt()
+    consumerProguardFiles("consumer-proguard-rules.pro")
+  }
+
+  lintOptions {
+    disable("GoogleAppIndexingWarning")
+    checkOnly("Interoperability")
+  }
+
+  buildFeatures.buildConfig = false
+}
+
+dependencies {
+  api(project(":leakcanary-object-watcher"))
+  api(project(":leakcanary-android-utils"))
+
+  implementation(libs.curtains)
+  implementation(libs.kotlin.stdlib)
+
+  testImplementation(libs.assertjCore)
+  testImplementation(libs.junit)
+  testImplementation(libs.kotlin.reflect)
+}
