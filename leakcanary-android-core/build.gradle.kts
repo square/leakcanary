735592f9d5ff2281657b88c040a9198fd8b60f63
Goooler <wangzongler@gmail.com>
2021-12-30 16:58:00 +0800

    Convert leakcanary-android-core's gradle to KTS

---
+++ leakcanary-android-core/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("com.vanniktech.maven.publish")
+}
+
+android {
+  resourcePrefix = "leak_canary_"
+  compileSdk = property("compileSdk").toString().toInt()
+  defaultConfig {
+    minSdk = property("minSdk").toString().toInt()
+    buildConfigField("String", "LIBRARY_VERSION", "\"${property("VERSION_NAME")}\"")
+    consumerProguardFiles("consumer-proguard-rules.pro")
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  lintOptions {
+    disable("GoogleAppIndexingWarning")
+    error("ObsoleteSdkInt")
+    checkOnly("Interoperability")
+  }
+}
+
+dependencies {
+  api(project(":shark-android"))
+  api(project(":leakcanary-object-watcher-android"))
+  api(project(":leakcanary-object-watcher-android-androidx"))
+  api(project(":leakcanary-object-watcher-android-support-fragments"))
+  implementation(project(":plumber-android"))
+  implementation(libs.kotlin.stdlib)
+
+  // Optional dependency
+  compileOnly(libs.androidX.work.runtime)
+  compileOnly(libs.androidX.work.multiprocess)
+
+  testImplementation(libs.assertjCore)
+  testImplementation(libs.junit)
+  testImplementation(libs.kotlin.reflect)
+  testImplementation(libs.mockito)
+  testImplementation(libs.mockitoKotlin)
+  androidTestImplementation(libs.androidX.test.espresso)
+  androidTestImplementation(libs.androidX.test.rules)
+  androidTestImplementation(libs.androidX.test.runner)
+  androidTestImplementation(project(":shark-hprof-test"))
+}
