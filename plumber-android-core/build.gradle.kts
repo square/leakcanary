30c0610709d3d8ecd0a79b5176a83946446dcbec
Goooler <wangzongler@gmail.com>
2021-12-31 10:48:09 +0800

    Convert plumber-android-core's gradle to KTS

---
+++ plumber-android-core/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("com.vanniktech.maven.publish")
+}
+
+android {
+  resourcePrefix = "leak_canary_plumber"
+  compileSdk = property("compileSdk").toString().toInt()
+  defaultConfig {
+    minSdk = property("minSdk").toString().toInt()
+    consumerProguardFiles("consumer-proguard-rules.pro")
+  }
+  buildFeatures.buildConfig = false
+  lintOptions {
+    disable("GoogleAppIndexingWarning")
+    error("ObsoleteSdkInt")
+    checkOnly("Interoperability")
+  }
+}
+
+dependencies {
+  api(project(":shark-log"))
+  api(project(":leakcanary-android-utils"))
+
+  implementation(libs.kotlin.stdlib)
+  // Optional dependency
+  compileOnly(libs.androidX.fragment)
+}
