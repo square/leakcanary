fae93f27e3e1724d980bc001f7cba87559dbfbc3
Goooler <wangzongler@gmail.com>
2021-12-31 11:04:24 +0800

    Convert shark's gradle to KTS

---
+++ shark/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  id("org.jetbrains.kotlin.jvm")
+  id("com.vanniktech.maven.publish")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+  api(project(":shark-graph"))
+
+  implementation(libs.kotlin.stdlib)
+  implementation(libs.okio2)
+
+  testImplementation(libs.assertjCore)
+  testImplementation(libs.junit)
+  testImplementation(libs.okio2)
+  testImplementation(project(":shark-hprof-test"))
+}
