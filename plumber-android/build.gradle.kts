a26e844e925d02ce27dae69f99bb44c104c3fdde
Goooler <wangzongler@gmail.com>
2021-12-31 10:46:40 +0800

    Convert plumber-android's gradle to KTS

---
+++ plumber-android/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("com.vanniktech.maven.publish")
+}
+
+android {
+  resourcePrefix = "leak_canary_plumber"
+  compileSdk = property("compileSdk").toString().toInt()
+  defaultConfig {
+    minSdk = property("minSdk").toString().toInt()
+    consumerProguardFiles("consumer-proguard-rules.pro")
+  }
+  buildFeatures.buildConfig = false
+  lintOptions {
+    disable("GoogleAppIndexingWarning")
+    error("ObsoleteSdkInt")
+    checkOnly("Interoperability")
+  }
+}
+
+dependencies {
+  api(project(":plumber-android-core"))
+
+  implementation(libs.kotlin.stdlib)
+}
