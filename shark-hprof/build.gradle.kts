8b45fc9d1bf07f0602cf539318fe1a7aa7ca5eaf
Goooler <wangzongler@gmail.com>
2021-12-31 11:21:54 +0800

    Convert shark-hprof's gradle to KTS

--- shark-hprof/build.gradle.kts
+++ shark-hprof/build.gradle.kts
@@ -3,23 +3,25 @@ plugins {
   id("com.vanniktech.maven.publish")
 }
 
-sourceCompatibility = JavaVersion.VERSION_1_8
-targetCompatibility = JavaVersion.VERSION_1_8
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 dependencies {
-  api project(':shark-log')
+  api(project(":shark-log"))
 
-  implementation libs.kotlin.stdlib
+  implementation(libs.kotlin.stdlib)
   // compileOnly ensures this dependency is not exposed through this artifact's pom.xml in Maven Central.
   // Okio is a required dependency, but we're making it required on the "shark" artifact which is the main artifact that
   // should generally be used. The shark artifact depends on Okio 2.x (ensure compatibility with modern Okio). Depending on 1.x here
   // enables us to ensure binary compatibility with Okio 1.x and allow us to use the deprecated (error level) Okio APIs to keep that
   // compatibility.
   // See https://github.com/square/leakcanary/issues/1624
-  compileOnly libs.okio1
-  testImplementation libs.okio1
+  compileOnly(libs.okio1)
+  testImplementation(libs.okio1)
 
-  testImplementation libs.assertjCore
-  testImplementation libs.junit
-  testImplementation project(':shark-test')
+  testImplementation(libs.assertjCore)
+  testImplementation(libs.junit)
+  testImplementation(project(":shark-test"))
 }
