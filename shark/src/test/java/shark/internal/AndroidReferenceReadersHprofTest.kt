f2c1bad5dbc24fe0a153aa594991e04450591680
Pierre-Yves Ricau <py@squareup.com>
2022-01-05 18:03:03 +0100

    Add support for AndroidX internal SafeIterableMap
    Before:
    
    ```
    │...
    ├─ com.example.leakcanary.MainActivity instance
    │    ↓ ComponentActivity.mLifecycleRegistry
    │                        ~~~~~~~~~~~~~~~~~~
    ├─ androidx.lifecycle.LifecycleRegistry instance
    │    ↓ LifecycleRegistry.mObserverMap
    │                        ~~~~~~~~~~~~
    ├─ androidx.arch.core.internal.FastSafeIterableMap instance
    │    ↓ SafeIterableMap.mEnd
    │                      ~~~~
    ├─ androidx.arch.core.internal.SafeIterableMap$Entry instance
    │    ↓ SafeIterableMap$Entry.mKey
    │                            ~~~~
    ├─ com.example.leakcanary.MainActivity$onCreate$MyObserver instance
    │    ↓ MainActivity$onCreate$MyObserver.leaking
    │                                       ~~~~~~~
    │...
    ```
    
    After:
    
    ```
    │...
    ├─ com.example.leakcanary.MainActivity instance
    │    ↓ ComponentActivity.mLifecycleRegistry
    │                        ~~~~~~~~~~~~~~~~~~
    ├─ androidx.lifecycle.LifecycleRegistry instance
    │    ↓ LifecycleRegistry.mObserverMap
    │                        ~~~~~~~~~~~~
    ├─ androidx.arch.core.internal.FastSafeIterableMap instance
    │    ↓ FastSafeIterableMap[key()]
    │                         ~~~~~~~
    ├─ com.example.leakcanary.MainActivity$onCreate$MyObserver instance
    │    ↓ MainActivity$onCreate$MyObserver.leaking
    │...
    ```

---
+++ shark/src/test/java/shark/internal/AndroidReferenceReadersHprofTest.kt
@@ -0,0 +1,31 @@
+package shark.internal
+
+import java.io.File
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import shark.HeapAnalysisSuccess
+import shark.LeakTraceReference.ReferenceType.ARRAY_ENTRY
+import shark.checkForLeaks
+
+class AndroidReferenceReadersHprofTest {
+
+  @Test fun `safe iterable map traversed as dictionary`() {
+    val hprofFile = "safe_iterable_map.hprof".classpathFile()
+    val analysis = hprofFile.checkForLeaks<HeapAnalysisSuccess>()
+    val leakTrace = analysis.applicationLeaks.single().leakTraces.single()
+
+    val mapReference =
+      leakTrace.referencePath.single { it.owningClassSimpleName == "FastSafeIterableMap" }
+    assertThat(mapReference.referenceName).isEqualTo("key()")
+    assertThat(mapReference.referenceType).isEqualTo(ARRAY_ENTRY)
+  }
+}
+
+fun String.classpathFile(): File {
+  val classLoader = Thread.currentThread()
+    .contextClassLoader
+  val url = classLoader.getResource(this)!!
+  return File(url.path)
+}
+
+
