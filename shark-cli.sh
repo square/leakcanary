410ba48415188d501752e83358eb39fd08c6bc1c
Pierre-Yves Ricau <py@squareup.com>
2023-05-18 13:06:12 -0700

    Organize modules by theme

--- shark-cli.sh
+++ shark-cli.sh
@@ -1,2 +1,2 @@
-./gradlew --quiet :shark-cli:installDist
-./shark-cli/build/install/shark-cli/bin/shark-cli "$@"
+./gradlew --quiet :shark:shark-cli:installDist
+./shark/shark-cli/build/install/shark-cli/bin/shark-cli "$@"
