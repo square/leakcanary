6d88c2497aa6581c4aa8ae2261632e6215dfb3c8
Pierre-Yves Ricau <py@squareup.com>
2023-05-19 17:19:38 -0700

    Remove deprecated AppWatcher.Config

--- object-watcher/object-watcher-android-core/src/test/java/leakcanary/AppWatcherTest.kt
+++ object-watcher/object-watcher-android-core/src/test/java/leakcanary/AppWatcherTest.kt
@@ -2,8 +2,6 @@ package leakcanary
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import kotlin.reflect.full.memberFunctions
-import kotlin.reflect.full.memberProperties
 
 class AppWatcherTest {
 
@@ -12,27 +10,4 @@ class AppWatcherTest {
       .describedAs("Ensure AppWatcher doesn't crash in JUnit tests")
       .isFalse()
   }
-
-  /**
-   * Validates that each field in [AppWatcher.Config] has a matching builder function
-   * in [AppWatcher.Config.Builder]
-   */
-  @Test fun `AppWatcher Config Builder matches AppWatcher Config`() {
-    assertThat(configProperties())
-      .containsExactlyInAnyOrderElementsOf(configBuilderFunctions())
-  }
-
-  private fun configBuilderFunctions() = AppWatcher.Config.Builder::class.memberFunctions
-    .filter { member ->
-      member.annotations.none { it is Deprecated }
-    }
-    .map { it.name }
-    .subtract(setOf("build", "equals", "hashCode", "toString"))
-
-  private fun configProperties() = AppWatcher.Config::class.memberProperties
-    .filter { member ->
-      // Ignore deprecated fields, we don't need builders for those
-      member.annotations.none { it is Deprecated }
-    }
-    .map { it.name }
 }
