a8395f43de518c07c0a81747d2089612bb2f03d2
Pierre-Yves Ricau <py@squareup.com>
2024-01-02 14:27:01 +0100

    Improve error message when android.os.Build class missing
    Related to #2580

--- shark/shark-android/src/main/java/shark/AndroidBuildMirror.kt
+++ shark/shark-android/src/main/java/shark/AndroidBuildMirror.kt
@@ -25,7 +25,12 @@ class AndroidBuildMirror(
      */
     fun fromHeapGraph(graph: HeapGraph): AndroidBuildMirror {
       return graph.context.getOrPut(AndroidBuildMirror::class.java.name) {
-        val buildClass = graph.findClassByName("android.os.Build")!!
+        // checkNotNull for more helpful message than NPE if this isn't an Android hprof.
+        // Not doing checkNotNull for other classes, if android.os.Build is there it's definitely
+        // an Android heap dump.
+        val buildClass = checkNotNull(graph.findClassByName("android.os.Build")) {
+          "android.os.Build class missing from heap dump, is this an Android heap dump?"
+        }
         val versionClass = graph.findClassByName("android.os.Build\$VERSION")!!
         val manufacturer = buildClass["MANUFACTURER"]!!.value.readAsJavaString()!!
         val sdkInt = versionClass["SDK_INT"]!!.value.asInt!!
