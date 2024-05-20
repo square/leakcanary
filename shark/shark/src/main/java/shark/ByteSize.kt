c5fd4dd94377b5eb559701296501ce44f8c3096e
Pierre-Yves Ricau <py@squareup.com>
2024-05-19 22:45:35 -0700

    Inline all the things

--- shark/shark/src/main/java/shark/ByteSize.kt
+++ shark/shark/src/main/java/shark/ByteSize.kt
@@ -6,17 +6,17 @@ package shark
 // DO NOT ADD A COMPANION OBJECT: a value class is supposed to be lightweight and its usage inlined
 // into few instructions. After adding a companion object, call sites get a lot more instructions.
 @JvmInline
-value class ByteSize internal constructor(
+value class ByteSize constructor(
   val inWholeBytes: Long
 ) : Comparable<ByteSize> {
 
-  val inWholeKilobytes: Long
+  inline val inWholeKilobytes: Long
     get() = inWholeBytes / BYTES_PER_KB
 
-  val inWholeMegabytes: Long
+  inline val inWholeMegabytes: Long
     get() = inWholeBytes / BYTES_PER_MB
 
-  val inWholeGigabytes: Long
+  inline val inWholeGigabytes: Long
     get() = inWholeBytes / BYTES_PER_GB
 
   override fun toString(): String {
@@ -30,16 +30,16 @@ value class ByteSize internal constructor(
 
   override operator fun compareTo(other: ByteSize) = inWholeBytes.compareTo(other.inWholeBytes)
 
-  operator fun plus(other: ByteSize): ByteSize =
+  inline operator fun plus(other: ByteSize): ByteSize =
     ByteSize(inWholeBytes = inWholeBytes + other.inWholeBytes)
 
-  operator fun minus(other: ByteSize): ByteSize =
+  inline operator fun minus(other: ByteSize): ByteSize =
     ByteSize(inWholeBytes = inWholeBytes - other.inWholeBytes)
 
-  operator fun times(other: ByteSize): ByteSize =
+  inline operator fun times(other: ByteSize): ByteSize =
     ByteSize(inWholeBytes * other.inWholeBytes)
 
-  operator fun div(other: ByteSize): ByteSize =
+  inline operator fun div(other: ByteSize): ByteSize =
     ByteSize(inWholeBytes / other.inWholeBytes)
 }
 
@@ -49,12 +49,12 @@ const val BYTES_PER_GB: Long = 1_000L * BYTES_PER_MB
 
 val ZERO_BYTES: ByteSize = ByteSize(0L)
 
-val Long.bytes get() = ByteSize(this)
-val Long.kilobytes get() = ByteSize(this * BYTES_PER_KB)
-val Long.megabytes get() = ByteSize(this * BYTES_PER_MB)
-val Long.gigabytes get() = ByteSize(this * BYTES_PER_GB)
+inline val Long.bytes get() = ByteSize(this)
+inline val Long.kilobytes get() = ByteSize(this * BYTES_PER_KB)
+inline val Long.megabytes get() = ByteSize(this * BYTES_PER_MB)
+inline val Long.gigabytes get() = ByteSize(this * BYTES_PER_GB)
 
-val Int.bytes get() = ByteSize(toLong())
-val Int.kilobytes get() = ByteSize(this * BYTES_PER_KB)
-val Int.megabytes get() = ByteSize(this * BYTES_PER_MB)
-val Int.gigabytes get() = ByteSize(this * BYTES_PER_GB)
+inline val Int.bytes get() = ByteSize(toLong())
+inline val Int.kilobytes get() = ByteSize(this * BYTES_PER_KB)
+inline val Int.megabytes get() = ByteSize(this * BYTES_PER_MB)
+inline val Int.gigabytes get() = ByteSize(this * BYTES_PER_GB)
--- shark/shark/src/main/java/shark/Retained.kt
+++ shark/shark/src/main/java/shark/Retained.kt
@@ -4,36 +4,40 @@ import shark.internal.packedWith
 import shark.internal.unpackAsFirstInt
 import shark.internal.unpackAsSecondInt
 
+/**
+ * Constructors can't be inlined so we used a function instead.
+ */
+inline fun Retained(
+  /**
+   * The minimum number of bytes which would be freed if all references to this object were
+   * released. Should not exceed [Int.MAX_VALUE] bytes.
+   */
+  heapSize: ByteSize,
+
+  /**
+   * The minimum number of objects which would be unreachable if all references to this object were
+   * released.
+   */
+  objectCount: Int,
+) = Retained(heapSize.inWholeBytes.toInt() packedWith objectCount)
+
 // DO NOT ADD A COMPANION OBJECT: a value class is supposed to be lightweight and its usage inlined
 // into few instructions. After adding a companion object, call sites get a lot more instructions.
 @JvmInline
-value class Retained private constructor(
-  private val packedValue: Long
+value class Retained @PublishedApi internal constructor(
+  @PublishedApi @JvmField
+  internal val packedValue: Long
 ) {
-  constructor(
-    /**
-     * The minimum number of bytes which would be freed if all references to this object were
-     * released. Should not exceed [Int.MAX_VALUE] bytes.
-     */
-    heapSize: ByteSize,
-
-    /**
-     * The minimum number of objects which would be unreachable if all references to this object were
-     * released.
-     */
-    objectCount: Int,
-  ) : this(heapSize.inWholeBytes.toInt() packedWith objectCount)
-
-  val heapSize: ByteSize
+  inline val heapSize: ByteSize
     get() = packedValue.unpackAsFirstInt.bytes
 
-  val objectCount: Int
+  inline val objectCount: Int
     get() = packedValue.unpackAsSecondInt
 
-  val isUnknown: Boolean
+  inline val isUnknown: Boolean
     get() = this == UNKNOWN_RETAINED
 
-  val isZero: Boolean
+  inline val isZero: Boolean
     get() = this == ZERO_RETAINED
 }
 
